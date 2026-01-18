c9a7c5594f977c9ff41b3a80c7383d58b1968838
R-Harivarshini <harivarshinir007@gmail.com>
2026-01-18 20:02:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+	public static void main(String[] args) {
+	    int num=6;
+	    if(num%2==0){
+	        System.out.println("the num is even ");
+	    }
+	    else{
+		System.out.println("the num is odd");
+	}
+}
